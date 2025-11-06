43a94fe06cf8a0ec04e7fdc71c49b58a9485211e
sesema98 <sergioserva14@gmail.com>
2025-11-05 22:19:20 -0500

    React router configurado con rutasy /entities aplicado

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,26 @@
+import { Link, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import Entities from './pages/Entities';
 
 export default function App() {
-  return <Home />;
-}
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container">
+          <Link className="navbar-brand" to="/">Rick&Morty</Link>
+          <div className="collapse navbar-collapse">
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item"><Link className="nav-link" to="/">Home</Link></li>
+              <li className="nav-item"><Link className="nav-link" to="/entities">Entities</Link></li>
+            </ul>
+          </div>
+        </div>
+      </nav>
 
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/entities" element={<Entities />} />
+      </Routes>
+    </>
+  );
+}
